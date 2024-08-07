338ceaa9c788fb906316de1a0bf960931864c9af
Hari prasad <hariprasad9693@gmail.com>
2024-08-07 12:14:49 +0530

    mlflow server setup in AWS completed

---
+++ dagshub-demo.py
@@ -0,0 +1,54 @@
+import dagshub
+import mlflow
+from mlflow.models import infer_signature
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+dagshub.init(repo_owner='kalehariprasad', repo_name='mlflow-aws-demo', mlflow=True)
+
+mlflow.set_tracking_uri('https://dagshub.com/kalehariprasad/mlflow-aws-demo.mlflow')
+
+# Load the Iris dataset
+X, y = datasets.load_iris(return_X_y=True)
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+with mlflow.start_run():
+    # Define the model hyperparameters
+    params = {
+        "solver": "lbfgs",
+        "max_iter": 1000,
+        "multi_class": "auto",
+        "random_state": 8888,
+    }
+
+    # Train the model
+    lr = LogisticRegression(**params)
+    lr.fit(X_train, y_train)
+
+    # Log model parameters
+    mlflow.log_params(params)
+
+    # Predict on the test set
+    y_pred = lr.predict(X_test)
+
+    # Calculate metrics
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred, average='weighted')
+    recall = recall_score(y_test, y_pred, average='weighted')
+    f1 = f1_score(y_test, y_pred, average='weighted')
+
+    # Log metrics
+    mlflow.log_metric('accuracy', accuracy)
+    mlflow.log_metric('precision', precision)
+    mlflow.log_metric('recall', recall)
+    mlflow.log_metric('f1_score', f1)
+
+    # Log model
+    mlflow.sklearn.log_model(lr ,"Logistic Regression")
